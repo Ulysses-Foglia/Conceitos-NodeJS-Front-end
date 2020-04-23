d48fc6c11bf63e3c282e1f60271076ac74c099f2
Ulysses-Foglia <ulyfoglia@gmail.com>
2020-04-23 11:28:52 -0300

    Listar, Adicionar e Remover Repositórios

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
+import moment from 'moment';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(() => {
+    api.get('repositories').then(response => { setRepositories(response.data); });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('repositories', {
+        url: "https://github.com/Rocketseat/umbriel",
+        title: `React - ${moment().format("DD/MM/YYYY hh:mm:ss")}`, 
+        //${Intl.DateTimeFormat('en-GB').format(Date.now()) não retornava hora
+        techs: ["Node", "Express", "TypeScript"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    const response = await api.delete(`repositories/${id}`);
+    
+    if (response.status === 204)
+    setRepositories(repositories.filter(repository => repository.id !== id));
+    
+    //const repoIndex = repositories.findIndex(repository => repository.id === id);
+    //foi trocada a função repositories.splice para repositories.filter
+    //o splice retirava o item selecionado de dentro de repositories, mas carregava na tela um elemento nulo
+    //o filter corrigiu isso. repoIndex era utilizado no repositories.splice
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {repositories.map(repository => <li key={repository.id}>{repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button>
+          </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+
     </div>
   );
 }
